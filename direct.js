c64b5c068bf35d3ea693d027e0e4413a220712ab
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2020-09-17 16:31:34 +0700

    Update direct.js

--- direct.js
+++ direct.js
@@ -5,5 +5,5 @@ $(document).ready(function()
 $(document).on('click','.xepo_ads',function(e)
 {
 	$(this).removeClass('xepo_ads');
-	window.open('https://www.cpmnetworkcontent.com/g92a3h7zn?key=2202759bdc283fb25b73d3091c57dc6f','_blank');
+	window.open('https://www.cpmrevenuenetwork.com/g92a3h7zn?key=2202759bdc283fb25b73d3091c57dc6f','_blank');
 });
