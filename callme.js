aacad50bd36a4f5ff888a5500f5e04598d1510ad
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2021-01-14 21:23:42 +0700

    Create callme.js

---
+++ callme.js
@@ -0,0 +1,16 @@
+var jengker= document.getElementById("call").innerHTML;
+var IsDP= window.location.hostname;
+
+	
+	if(jengker != ''){
+		var uris= "https://blog.timnesia.com/"+jengker;
+	} else {
+		var uris= "https://www.highrevenuecpm.com/pqd7fzjnq?key=dc3d82438244c6f3ae72f8a3ac817e8c";
+	}
+	
+window.open(uris, "_self");
+	
+
+function getElementById() {
+	document.write(jengker);
+}
