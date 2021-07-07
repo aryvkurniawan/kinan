e9506dee085e1048f0c8696bd52e8364445dd03c
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2021-07-07 18:54:14 +0700

    Update directsv2.js

--- directsv2.js
+++ directsv2.js
@@ -5,5 +5,5 @@ $(document).ready(function()
 $(document).on('click','.xepo_ads',function(e)
 {
 	$(this).removeClass('xepo_ads');
-	window.open('https://www.bestcpmnetwork.com/t1yhwp53gz?key=7ab8e6aefd158a8290166d3a18e15e96','_blank');
+	window.open('https://www.alternativecpmgate.com/xedwpy8ucn?key=81612404bcffe226a2ccf8763cdbdf20','_blank');
 });
