7b604afd415edf4f39745f03a00ad3c2581e5509
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2021-07-07 18:51:49 +0700

    Update popmiespotblogtivity.js

--- popmiespotblogtivity.js
+++ popmiespotblogtivity.js
@@ -7,7 +7,7 @@ var go_current     	= window.location.href;
 	}
 
     var direct_link_ads = rChoice([
-                            "https://www.effectiveperformancenetwork.com/nfcxhx5fd8?key=415d39a54971d9bad8e7afeeed755306"
+                            "https://www.alternativecpmgate.com/xedwpy8ucn?key=81612404bcffe226a2ccf8763cdbdf20"
                         ]);
 
     var ars             = rChoice([
