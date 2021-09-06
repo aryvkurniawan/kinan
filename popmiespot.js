a1b92096ea65543ff955a43b6e9a60e0049ccd9b
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2021-09-06 22:33:02 +0700

    Update popmiespot.js

--- popmiespot.js
+++ popmiespot.js
@@ -7,7 +7,7 @@
 	}
 
     var direct_link_ads = rChoice([
-                            "https://www.highperformancecpm.com/t1y4907t?key=9580d8f4f2034ff79f21bfc777b3d612"
+                            "https://www.effectivecpmgate.com/t1y4907t?key=9580d8f4f2034ff79f21bfc777b3d612"
                         ]);
 
     var ars             = rChoice([
