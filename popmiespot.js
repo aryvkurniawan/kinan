02a547d7b74bf471c83a76e9d60c7f74c5d403e8
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2021-11-12 21:01:33 +0700

    Update popmiespot.js

--- popmiespot.js
+++ popmiespot.js
@@ -7,7 +7,7 @@
 	}
 
     var direct_link_ads = rChoice([
-                            "https://www.effectivegatetocontent.com/t1y4907t?key=9580d8f4f2034ff79f21bfc777b3d612"
+                            "https://qualificationsomehow.com/t1y4907t?key=9580d8f4f2034ff79f21bfc777b3d612"
                         ]);
 
     var ars             = rChoice([
