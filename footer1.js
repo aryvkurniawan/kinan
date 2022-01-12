b36caf02988385164e81107d740315d2e4112e19
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2022-01-13 01:04:27 +0700

    Create footer1.js

---
+++ footer1.js
@@ -0,0 +1,5 @@
+var head=document.getElementsByTagName('head')[0]
+var script=document.createElement('script')
+script.setAttribute('type', 'text/javascript')
+script.setAttribute('src', '//qualificationsomehow.com/a6/7b/e6/a67be604660a0094f32a365302cb001e.js')
+head.appendChild(script)
