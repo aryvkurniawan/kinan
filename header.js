2813b2c63919ad687d8df2d873a2beeffa7a2dcb
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2022-12-03 19:32:19 +0700

    Update header.js

--- header.js
+++ header.js
@@ -1,1 +1,8 @@
-
+atOptions = {
+		'key' : '156e5292d96db99357a65f487d46e40f',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/156e5292d96db99357a65f487d46e40f/invoke.js"></scr' + 'ipt>');
