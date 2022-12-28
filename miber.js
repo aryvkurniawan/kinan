33e874b1c79fb1a50fcbba5773b1d3b3ed49b821
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2022-12-28 21:33:19 +0700

    Create miber.js

---
+++ miber.js
@@ -0,0 +1,8 @@
+atOptions = {
+        'key' : '911a38788276e7aee5fbcb2e12f4d8f2',
+        'format' : 'iframe',
+        'height' : 250,
+        'width' : 300,
+        'params' : {}
+    };
+    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/911a38788276e7aee5fbcb2e12f4d8f2/invoke.js"></scr' + 'ipt>');
