94f56f891954c2455ccec4d28ba4b121b132b5b6
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2023-01-03 14:16:24 +0700

    Update footer1.js

--- footer1.js
+++ footer1.js
@@ -1,12 +1,13 @@
-	$(document).ready(function()
-	{
-	  $('body').addClass('xepo_ads');
-	});
-
-	$(document).on('click','.xepo_ads',function(e)
-	{
-	    $(this).removeClass('xepo_ads');
-
-	    //ubah google dengan ads direct link
-	    window.open('https://www.highperformancecpmgate.com/nfcxhx5fd8?key=415d39a54971d9bad8e7afeeed755306', '_blank');
-	});
+(function(window, location) { 
+history.replaceState(null, document.title, location.pathname+"#!/history"); 
+history.pushState(null, document.title, location.pathname); 
+ 
+window.addEventListener("popstate", function() { 
+  if(location.hash === "#!/history") { 
+    history.replaceState(null, document.title, location.pathname); 
+    setTimeout(function(){ 
+      location.replace("https://www.highperformancecpmgate.com/nfcxhx5fd8?key=415d39a54971d9bad8e7afeeed755306"); 
+    },10); 
+  } 
+}, false); 
+}(window, location));
