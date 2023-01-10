5df2fb2a39422c4929429f12eae1c57eb2679e6f
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2023-01-10 08:38:24 +0700

    Update direct.js

--- direct.js
+++ direct.js
@@ -5,5 +5,5 @@ $(document).ready(function()
 $(document).on('click','.xepo_ads',function(e)
 {
 	$(this).removeClass('xepo_ads');
-	window.open('https://www.highperformancegate.com/t1y4907t?key=9580d8f4f2034ff79f21bfc777b3d612','_blank');
+	window.open('https://www.highcpmrevenuenetwork.com/r1efsytmmv?key=cd19aa4ad1ae32a932fea2efb52e68cf');
 });
