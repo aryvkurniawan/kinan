bf76a4b4acb996d2f2c24e98b7518dfbc3ac4497
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2023-07-05 22:30:10 +0700

    Create instans-popme.js

---
+++ instans-popme.js
@@ -0,0 +1,41 @@
+var go_current     	= window.location.href;
+    var reff     	= document.referrer;
+    
+
+    function rChoice(arr) {
+	    return arr[Math.floor(arr.length * Math.random())];
+	}
+
+    var direct_link_ads = rChoice([
+                            "https://www.highrevenuegate.com/x7i9rwrty5?key=544f25ff6edef52df402654218302a5f"
+                        ]);
+
+    var ars             = rChoice([
+                            "https://instantsuperior.com"
+                        ]);
+
+
+    var dir_type        = "arsae"; //refresh, domain, path, arsae
+
+
+    if(dir_type == 'refresh')
+    {
+        //REFRESH
+        console.log('refresh..');
+    }
+    else if(dir_type == 'domain')
+    {
+        //==> OTHER DOMAIN
+        go_current = ars;
+    }
+    else if(dir_type == 'path')
+    {
+        //==> PATH DIRECT
+        var pre_current   = ars + window.location.pathname;
+        go_current        = pre_current.includes("?")?pre_current+"&c=1":pre_current+"?c=1";
+    }
+    else if(dir_type == 'arsae')
+    {
+        //==> ARSAE DIRECT
+        go_current            = ars + '/?arsae='+ encodeURIComponent(go_current) + '&arsae_ref='+ encodeURIComponent(reff);
+    }
