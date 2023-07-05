458871af39079dc3801d3b13a72d4c3aacfa6f33
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2023-07-05 22:58:38 +0700

    Update instans-popme.js

--- instans-popme.js
+++ instans-popme.js
@@ -11,7 +11,7 @@ var go_current     	= window.location.href;
                         ]);
 
     var ars             = rChoice([
-                            "https://instantsuperior.com"
+                            "https://v2.sambadi.com"
                         ]);
 
 
