e2c3bdcf3032f82b226834bb20554e19cebba08c
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2024-04-22 12:40:03 +0700

    Create primary-24.js

---
+++ primary-24.js
@@ -0,0 +1,8 @@
+atOptions = {
+        'key' : 'a0d240a1f8e89e798b2044b43265e653',
+        'format' : 'iframe',
+        'height' : 250,
+        'width' : 300,
+        'params' : {}
+    };
+    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topcreativeformat.com/a0d240a1f8e89e798b2044b43265e653/invoke.js"></scr' + 'ipt>');
