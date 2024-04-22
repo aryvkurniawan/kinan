0fb7e373405fc9cfcc75a0e69c6722d4a2ecd7dd
aryvkurniawan <41510833+aryvkurniawan@users.noreply.github.com>
2024-04-22 12:47:59 +0700

    Create load-24.js

---
+++ load-24.js
@@ -0,0 +1,8 @@
+atOptions = {
+        'key' : 'c8f5c08adfc7e735f1a878822438e525',
+        'format' : 'iframe',
+        'height' : 250,
+        'width' : 300,
+        'params' : {}
+    };
+    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topcreativeformat.com/c8f5c08adfc7e735f1a878822438e525/invoke.js"></scr' + 'ipt>');
